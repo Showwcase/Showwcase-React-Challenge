42fa12645317beac792bcad7ab783864f4ae1090
rilmubarak <mubaraqhere@gmail.com>
2023-07-27 20:56:40 +0700

    UPDATE: input is updated

--- showwcase-app/src/components/UI/Input/Input.styled.tsx
+++ showwcase-app/src/components/UI/Input/Input.styled.tsx
@@ -1,36 +1,34 @@
 import styled from "styled-components"
 
-interface Props {
-  bgColor?: string;
-}
-
-const Button = styled.div<Props>`
+export default styled.div`
   margin-top: 12px;
+  font-size: 14px;
+  position: relative;
   
   label {
+    font-family: "Arial";
     font-weight: 600;
-    font-size: 18px;
     color: #d0d5dd;
     display: block;
     margin-bottom: 8px;
   }
 
   input {
-    font-size: 16px;
     font-weight: 400;
-    width: 250px;
+    width: 100%;
+    min-width: 250px;
     color: #344054;
-    background-color: ${(props) => props.bgColor || "#ffffff"};
-    border: 1px solid #d0d5dd;
+    color: #ffffff;
+    background-color: transparent;
+    border: 1px solid #2d2e2f;
     box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-    border-radius: 8px;
+    border-radius: 6px;
     padding: 10px 14px;
   }
 
   input::placeholder {
-    font-size: 16px;
     font-weight: 400;
-    color: #667085;
+    color: #6f7990;
   }
 
   /* Input field focus */
@@ -45,6 +43,74 @@ const Button = styled.div<Props>`
     margin-left: 12px;
     margin-top: 4px;
   }
-`
 
-export default Button;
+  .loader {
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .options {
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: none;
+    max-height: 15em;
+    overflow-y: auto;
+    border: .05em solid #777;
+    border-radius: .25em;
+    width: 100%;
+    left: 0;
+    top: calc(100% + .25em);
+    background-color: #171718;
+    z-index: 100;
+  }
+
+  .options.show {
+    display: block;
+  }
+
+  .option {
+    padding: 10px 14px;
+    cursor: pointer;
+  }
+
+  .option.selected {
+    background-color: #2c3766;
+  }
+
+  .option.highlighted {
+    background-color: #455698;
+    color: white
+  }
+
+  .option-badge {
+    display: flex;
+    align-items: center;
+    border: .05em solid #777;
+    border-radius: .25em;
+    padding: .15em .25em;
+    gap: .25em;
+    cursor: pointer;
+    background: none;
+    outline: none;
+  }
+
+  .option-badge:hover,
+  .option-badge:focus {
+    background-color: hsl(0, 100%, 90%);
+    border-color: hsl(0, 100%, 50%);
+  }
+
+  .option-badge:hover > .remove-btn,
+  .option-badge:focus > .remove-btn{
+    color: hsl(0, 100%, 50%);
+  }
+
+  .option-badge > .remove-btn {
+    font-size: 1.25em;
+    color: #777;
+  }
+`
--- showwcase-app/src/components/UI/Input/index.tsx
+++ showwcase-app/src/components/UI/Input/index.tsx
@@ -1,39 +1,113 @@
 import InputStyle from 'src/components/UI/Input/Input.styled';
-import React, { ChangeEvent, FC } from "react";
+import React, { ChangeEvent, FC, useRef, useState } from "react";
+import { SelectOption, SchoolType } from "src/utils/types";
+import { useContainerRef } from 'src/hooks/useContainerRef';
 
 interface Props {
-  type: 'text' | 'number' | 'email' | 'password'
-  name: string
-  value: string | number
-  label?: string
-  placeholder?: string
-  error?: boolean
-  disabled?: boolean
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
-}
+  type: string;
+  name: string;
+  value: string | number;
+  label?: string;
+  error?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+  isLoading?: boolean;
+  isAutocomplete?: boolean;
+  autocompleteInput?: object;
+  setAutocompleteInput?: React.Dispatch<React.SetStateAction<any>>;
+  options?: SelectOption[] | SchoolType[] | null;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
 const Input: FC<Props> = ({
   type,
-  label,
-  value,
   name,
-  placeholder,
+  value,
+  label,
   error,
   disabled,
+  placeholder,
+  isLoading,
+  isAutocomplete,
+  autocompleteInput,
+  setAutocompleteInput,
+  options,
   onChange,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [optionsMatch, setOptionsMatch] = useState<SelectOption[] | SchoolType[]>([]);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+  const containerRef = useRef<HTMLInputElement>(null);
+
+  //handle autocomplete
+  const handleAutocomplete = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+
+    if (setAutocompleteInput) {
+      setAutocompleteInput({...autocompleteInput, [name]: val});
+    };
+
+    if (options) {
+      setIsOpen(true);
+
+      // Filter the options based on the input value
+      const filtered = options.filter((option) =>
+        option.name.toLowerCase().includes(val.toLowerCase())
+      );
+      
+      setOptionsMatch(filtered);
+    } else {
+      setIsOpen(false);
+    };
+  };
+
+  //handle select options
+  const selectOption = (val: SelectOption) => {
+    if (setAutocompleteInput) {
+      setAutocompleteInput({...autocompleteInput, [name]: val.name});
+    };
+  };
+
+  //hook for handle container ref to allow users to choose the options by using their keyboard
+  useContainerRef(containerRef, isOpen, setIsOpen, highlightedIndex, setHighlightedIndex, optionsMatch, selectOption);
+
   return (
     <InputStyle>
-      <label htmlFor={label}>{label}</label>
+      <label htmlFor={name}>{label}</label>
       <input
-        type={type}
-        id={label}
-        value={value}
+        id={name}
         name={name}
-        placeholder={placeholder}
-        onChange={onChange}
+        type={type}
+        value={value}
         disabled={disabled}
+        placeholder={placeholder}
+        onChange={isAutocomplete? handleAutocomplete : onChange}
+        ref={containerRef}
+        tabIndex={0}
       />
+
+      { (isLoading || optionsMatch.length > 0) &&
+        <ul className={`options ${isOpen ? "show" : ""}`}>
+          { isLoading ? <div className='loader'>Loading...</div> :
+            optionsMatch.map((option, index) => (
+              <li
+                onClick={e => {
+                  e.stopPropagation();
+                  setIsOpen(false);
+                  selectOption(option);
+                }}
+                onMouseEnter={() => setHighlightedIndex(index)}
+                key={index+1}
+                className={`option ${index === highlightedIndex ? "highlighted" : ""}`}
+              >
+                {option.name}
+              </li>
+            ))
+          }
+        </ul>
+      }
+
       {error && <p className="error">Input filed can't be empty!</p>}
     </InputStyle>
   )
